b4a57be6c3b2160d999ff4d3181f539d13a73832
Marvin Gorenflo <gorenflo.marvin@web.de>
2025-05-01 00:26:21 +0200

    Added ef core package references and di class

---
+++ DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Korrekturmanagementsystem.Data;
+using Korrekturmanagementsystem.Repositories;
+using Korrekturmanagementsystem.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Korrekturmanagementsystem;
+
+public static class DependencyInjection 
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+
+        return services;
+    }
+
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        return services;
+    }
+}
