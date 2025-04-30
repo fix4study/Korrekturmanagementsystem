534e1f0da945e4633d657824c6ab09b4142bf5dc
Marvin Gorenflo <gorenflo.marvin@web.de>
2025-05-01 00:25:16 +0200

    Added ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Korrekturmanagementsystem.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Korrekturmanagementsystem.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Report> Reports { get; set; } = default!;
+    public DbSet<ReportType> ReportTypes { get; set; } = default!;
+    public DbSet<Material> Materials { get; set; } = default!;
+    public DbSet<MaterialType> MaterialTypes { get; set; } = default!;
+    public DbSet<Status> Statuses { get; set; } = default!;
+    public DbSet<Priority> Priorities { get; set; } = default!;
+    public DbSet<User> Users { get; set; } = default!;
+    public DbSet<ReportHistory> ReportHistories { get; set; } = default!;
+    public DbSet<Comment> Comments { get; set; } = default!;
+    public DbSet<Attachment> Attachments { get; set; } = default!;
+    public DbSet<Role> Roles { get; set; } = default!;
+    public DbSet<ReportTag> ReportTags { get; set; } = default!;
+    public DbSet<Tag> Tags { get; set; } = default!;
+    public DbSet<Course> Courses { get; set; } = default!;
+}
