e56650cde969ee84fce877b90125c47bd86258e8
Marvin Gorenflo <gorenflo.marvin@web.de>
2025-05-01 00:25:49 +0200

    Added generic repository with interface

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,75 @@
+﻿using Korrekturmanagementsystem.Data;
+using Korrekturmanagementsystem.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Korrekturmanagementsystem.Repositories;
+
+public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
+{
+    protected readonly ApplicationDbContext context;
+    protected readonly DbSet<TEntity> dbSet;
+
+    public GenericRepository(ApplicationDbContext context)
+    {
+        this.context = context;
+        this.dbSet = context.Set<TEntity>();
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> GetAsync(
+        Expression<Func<TEntity, bool>> filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        string includeProperties = "")
+    {
+        IQueryable<TEntity> query = dbSet;
+
+        if (filter != null)
+        {
+            query = query.Where(filter);
+        }
+
+        foreach (var includeProperty in includeProperties.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+        {
+            query = query.Include(includeProperty);
+        }
+
+        return orderBy != null ? await orderBy(query).ToListAsync() : await query.ToListAsync();
+    }
+
+    public virtual async Task<TEntity> GetByIdAsync(object id)
+    {
+        return await dbSet.FindAsync(id);
+    }
+
+    public virtual async Task InsertAsync(TEntity entity)
+    {
+        await dbSet.AddAsync(entity);
+    }
+
+    public virtual void Update(TEntity entity)
+    {
+        dbSet.Attach(entity);
+        context.Entry(entity).State = EntityState.Modified;
+    }
+
+    public virtual void Delete(object id)
+    {
+        var entity = dbSet.Find(id);
+        if (entity != null)
+        {
+            Delete(entity);
+        }
+    }
+
+    public virtual void Delete(TEntity entity)
+    {
+        if (context.Entry(entity).State == EntityState.Detached)
+        {
+            dbSet.Attach(entity);
+        }
+
+        dbSet.Remove(entity);
+    }
+}
+
+
