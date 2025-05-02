3d7380e9277b72868e49413a960ed9cdd8e99b82
Marvin Gorenflo <gorenflo.marvin@web.de>
2025-05-02 23:10:33 +0200

    Further implementation for auth

---
+++ Endpoints/LoginEndpoint.cs
@@ -0,0 +1,48 @@
+﻿using Korrekturmanagementsystem.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Korrekturmanagementsystem.Endpoints
+{
+    public static class LoginEndpoint
+    {
+        public static void MapLoginEndpoint(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("/account/login", async (
+                HttpContext http,
+                IUserService userService,
+                IPasswordService passwordService,
+                [FromForm] string username,
+                [FromForm] string password
+            ) =>
+            {
+                var user = await userService.GetUserByUsernameAsync(username);
+                if (user is null || !passwordService.VerifyPassword(user.HashedPassword, password))
+                {
+                    return Results.Redirect("/account/login?error=true");
+                }
+
+                var claims = new List<Claim>
+                {
+                    new(ClaimTypes.Name, user.Username),
+                    new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new(ClaimTypes.Role, user.RoleName)
+                };
+
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+
+                await http.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal,
+                        new AuthenticationProperties
+                        {
+                            IsPersistent = true,
+                            ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(30)
+                        });
+
+                return Results.Redirect("/");
+            }).AllowAnonymous().DisableAntiforgery();
+        }
+    }
+}
---
+++ Endpoints/LogoutEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Korrekturmanagementsystem.Endpoints
+{
+    public static class LogoutEndpoint
+    {
+        public static void MapLogoutEndpoint(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("/account/logout", async (HttpContext http) =>
+            {
+                await http.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                return Results.Redirect("/account/login");
+            });
+        }
+    }
+}
