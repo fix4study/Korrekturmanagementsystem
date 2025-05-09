9a4196553bf3013615069b599c70d273fc47a56a
Marvin Gorenflo <gorenflo.marvin@web.de>
2025-05-09 13:56:11 +0200

    Optimized Register.razor + added feedback

--- Dtos/CreateUserDto.cs
+++ Dtos/CreateUserDto.cs
@@ -1,10 +1,21 @@
-﻿namespace Korrekturmanagementsystem.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Korrekturmanagementsystem.Dtos;
 
 public class CreateUserDto
 {
+    [Required(ErrorMessage = "Benutzername ist erforderlich.")]
+    [StringLength(50, ErrorMessage = "Benutzername darf maximal 50 Zeichen enthalten.")]
     public string Username { get; set; } = default!;
+
+    [Required(ErrorMessage = "E-Mail ist erforderlich.")]
+    [EmailAddress(ErrorMessage = "Bitte eine gültige E-Mail-Adresse eingeben.")]
     public string Email { get; set; } = default!;
+
+    [Required(ErrorMessage = "Passwort ist erforderlich.")]
+    [MinLength(6, ErrorMessage = "Das Passwort muss mindestens 6 Zeichen lang sein.")]
     public string Password { get; set; } = default!;
+
+    [Required(ErrorMessage = "Bitte eine Rolle auswählen.")]
     public Guid StakeholderRoleId { get; set; }
-    public DateTime CreatedAt { get; set; }
 }
